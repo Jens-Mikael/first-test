801b873fa536d05fd4d9a300246cdd2fae4b18e6
Jens-Mikael <jensmikael.stjernberg@gmail.com>
2023-04-06 16:01:47 +0300

    built in test

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className='text-green-400'>Hello world</div>
+      <div className="text-green-400">Hello world</div>
+      <div className=" text-9xl text-sky-400">Built in test</div>
     </main>
-  )
+  );
 }
